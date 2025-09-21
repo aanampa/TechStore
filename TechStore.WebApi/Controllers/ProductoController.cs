9b95b8418c4dde1fa4b4aa36754dc72082fabf75
Antonio Anampa <aanampa@editoraperu.com.pe>
2025-09-21 17:16:57 -0500

    Agregar proyecto WebApi

---
+++ TechStore.WebApi/Controllers/ProductoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechStore.Application.DTOs;
+using TechStore.Application.Interfaces;
+using TechStore.Application.Services;
+
+namespace TechStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly ILogger<ProductoController> _logger;
+
+        private readonly IProductService _productService;
+
+        public ProductoController(ILogger<ProductoController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProductos()
+        {
+            try
+            {
+                var products = await _productService.GetAllProductsAsync();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
+    }
+}

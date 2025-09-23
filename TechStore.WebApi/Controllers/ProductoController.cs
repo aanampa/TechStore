db7236649ca95323a6af82da8e5757ff03baf52b
Maier Cipriano <mcipriano.b@gmail.com>
2025-09-22 21:43:00 -0500

    Cambios de CRUD

--- TechStore.WebApi/Controllers/ProductoController.cs
+++ TechStore.WebApi/Controllers/ProductoController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using TechStore.Application.DTOs;
 using TechStore.Application.Interfaces;
 using TechStore.Application.Services;
+using TechStore.Domain.Entities;
 
 namespace TechStore.WebApi.Controllers
 {
@@ -33,5 +36,69 @@ namespace TechStore.WebApi.Controllers
                 return StatusCode(500, $"Error interno del servidor: {ex.Message}");
             }
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetProducto(Guid id)
+        {
+            try
+            {
+                var product = await _productService.GetProductByIdAsync(id);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> PostProducto(ProductDto producto)
+        {
+            try
+            {
+                producto.Id = Guid.NewGuid();
+
+                var product = await _productService.CreateProductAsync(producto);
+                
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductDto>> PutProducto(Guid id, ProductDto producto)
+        {
+            try
+            {
+                if (id != producto.Id)
+                    return BadRequest();
+
+                var product = await _productService.UpdateProductAsync(id, producto);
+
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProducto(Guid id)
+        {
+            try
+            {
+                var product = await _productService.DeleteProductAsync(id);
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
+
     }
 }
